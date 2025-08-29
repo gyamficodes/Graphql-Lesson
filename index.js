baed146a6355b58ad87559e9ca4af841cc9c994c
gyamficodes <gyamfijohn08@gmail.com>
2025-08-29 13:45:08 +0000

    Install Apollo Server and GraphQL dependencies; set up initial GraphQL server structure

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
-import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
 
+//types
+import { typeDefs } from "./schema.js";
+//db
+import { db } from "./db.js";
 
-const server  = new ApolloServer({
+const resolver = {
+  Query: {
+    games: () => db.games,
+    reviews: () => db.reviews,
+    authors: () => db.authors,
+  },
+};
 
-})
+const server = new ApolloServer({
+  typeDefs,
+  resolver,
+});
 
-
-const { url} = await startStandaloneServer(server, {
-    listen: { port: 4000 }
-})
+const { url } = await startStandaloneServer(server, {
+  listen: { port: 4000 },
+});
 
 console.log(`Server ready at ${url}`);
---
+++ schema.js
@@ -0,0 +1,32 @@
+export const typeDefs = `#graphql
+  type Game {
+    id: ID!
+    title: String!
+    platform: [String!]!
+    # reviews: [Review!]! # Relationship: A Game can have many Reviews
+  }
+  
+  type Review {
+    id: ID!
+    rating: Int!
+    content: String!
+    # author: Author!   # Relationship: A Review is written by one Author
+  }
+
+  type Author {
+    id: ID!
+    name: String!
+    verified: Boolean!
+    # reviews: [Review!]! # Relationship: An Author can have many Reviews
+  }
+
+  type Query {
+    # Query to get all arrays of items
+    games: [Game]
+    reviews: [Review]
+    authors: [Author]
+
+    # Query to get a specific item by ID (optional but very useful)
+
+  }
+`;
